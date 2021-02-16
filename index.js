039f0d763856c4117162050f82795d709e6269e0
Charles Lai <charlesangeloplai@gmail.com>
2021-02-16 18:47:26 -0500

    commit tree e5682db7bd86e2c07532568b3c01d8f32147ffc4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+  let cards = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards;
+}
+
+function countDown(startIndex) {
+  while (startIndex > 0) {
+    console.log(startIndex);
+    startIndex--;
+  }
+  console.log(startIndex);
+}
